482ce568dcf421451b75f7b6d964b1fe53096da1
pedrodawybida <pedrodawybida@icloud.com>
2024-08-29 21:12:23 -0300

    segunda versão

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-import os
 from telethon import TelegramClient, events
+import senhas
 
 sessao = 'repassar_mensagem'
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
 
 def alterar_titulo(mensagem, novo_titulo):
+    # Separa a mensagem em linhas
     linhas = mensagem.split('\n')
+    
+    # Substitui a primeira linha (título) pelo novo título
     linhas[0] = novo_titulo
-    return '\n'.join(linhas)
-
-async def enviar_mensagem(event, client):
-    mensagem = event.raw_text
-    linhas = mensagem.split('\n')
-
-    if linhas[0].startswith("📊SALA VIP | Mateus Trader📊"):
-        if len(linhas) > 1 and linhas[1].startswith("🔔 Resultados para o dia"):
-            return
-        
-        nova_mensagem = alterar_titulo(mensagem, "📊SALA VIP📊")
-        await client.send_message(4587247538, nova_mensagem)
+    
+    # Reune as linhas de volta em uma string
+    nova_mensagem = '\n'.join(linhas)
+    
+    return nova_mensagem
 
 def main():
-    client = TelegramClient(sessao, api_id, api_hash)
+    client = TelegramClient(sessao, senhas.api_id, senhas.api_hash)
 
     @client.on(events.NewMessage(chats=[4507534222]))
-    async def handler(event):
-        await enviar_mensagem(event, client)
+    async def enviar_mensagem(event):
+        mensagem = event.raw_text
+        
+        # Verifica se a mensagem contém o título que você deseja modificar
+        if mensagem.startswith("📊SALA VIP | Mateus Trader📊"):
+            # Altera o título para "SALA VIP"
+            nova_mensagem = alterar_titulo(mensagem, "SALA VIP")
+            
+            # Envia a mensagem alterada para o outro chat
+            await client.send_message(4587247538, nova_mensagem)
+        else:
+            # Caso não seja a mensagem alvo, repassa sem alterações
+            await client.send_message(4587247538, mensagem)
 
     client.start()
     client.run_until_disconnected()
---
+++ senhas.py
@@ -0,0 +1,2 @@
+api_id = 28654898
+api_hash = '927d00353bc7fb491fbf10ea866fb2c9'
